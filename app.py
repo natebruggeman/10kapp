321acf114147531e56120c4173685ab567fa8776
Nate Bruggeman <njbruggeman@gmail.com>
2019-11-20 16:23:55 -0600

    set up login manager and secret key

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify, g 
+from flask_cors import CORS
+from flask_login import LoginManager
+
+
+import models
+
 
 DEBUG = True
 PORT = 8000
 
 
 app = Flask(__name__)
+app.secret_key = "Secret Key Nate rules, Conrad drools"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+
+@login_manager.user_loader
+def load_user(userid):
+	try:
+		return models.User.get(models.User.id == userid)
+	except models.DoesNotExist:
+		return None	
+
+
+@app.before_request
+def before_request():
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
 
 
 @app.route('/')
 def index():
     return 'hello Nate and Conrad'
+
+
+
 
 
 if __name__ == '__main__':
