d7ecf7fb6b4bdebe47389b1f1feda968470d7370
Nate Bruggeman <njbruggeman@gmail.com>
2019-11-20 16:12:17 -0600

    created User model

--- models.py
+++ models.py
@@ -1,6 +1,19 @@
 from peewee import *
 
+from flask_login import UserMixin
+
+
 DATABASE = SqliteDatabase('skills.sqlite')
+
+
+class User(UserMixin, Model):
+    username = CharField(unique = True)
+    email = CharField(unique = True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+
 
 
 class Skill(Model):
