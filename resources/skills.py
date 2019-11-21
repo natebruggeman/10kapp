2223165f77a95c928c122cbea5df3ac2ba3a88d9
Nate Bruggeman <njbruggeman@gmail.com>
2019-11-20 21:57:15 -0600

    delete route functioning

--- resources/skills.py
+++ resources/skills.py
@@ -25,14 +25,14 @@ def create_skills():
 	skill = models.Skill.create(**payload)
 	print(skill.__dict__)
 	skill_dict = model_to_dict(skill)
-	return jsonify(data=skill_dict, status={"code": 201, "message": "Success"})
+	return jsonify(data=skill_dict, status={"code": 200, "message": "Success"}) 
 
 
 # show dat route
 @skill.route('/<id>', methods=["GET"])
 def get_one_skill(id):
     skill = models.Skill.get_by_id(id)
-    return jsonify(data=model_to_dict(skill), status={"code": 200, "message": "Success"})
+    return jsonify(data=model_to_dict(skill), status={"code": 200, "message": "Success"}) 
 
 
 #update
@@ -43,10 +43,15 @@ def update_skill(id):
     query.execute()
     skill = models.Skill.get_by_id(id)
     skill_dict = model_to_dict(skill)
-    return jsonify(data=skill_dict, status={'code': 200, 'message': 'Updated successfully'})
+    return jsonify(data=skill_dict, status={'code': 200, 'message': 'Updated successfully'}) 
 
 
-
+# deletion route 
+@skill.route('/<id>', methods=["Delete"])
+def delete_skill(id):
+    query = models.Skill.delete().where(models.Skill.id==id)
+    query.execute()
+    return jsonify(data='Resource Deleted', status={"code": 200, "message": "Success deletion"}) 
 
 
 
